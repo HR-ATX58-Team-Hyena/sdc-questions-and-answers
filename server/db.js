1d7ef5043514693778c9fb246a14fe8a853ca722
Cory Ellerbroek <cory.ellerbroek@gmail.com>
2021-09-30 09:51:11 -0500

    Extracted login data to gitignored config file

--- server/db.js
+++ server/db.js
@@ -1,11 +1,6 @@
 const { Client } = require("pg");
 const fs = require("fs");
 const fastcsv = require("fast-csv");
+const config = require("./config.js");
 
-const client = new Client({
-  host: "localhost",
-  user: "postgres",
-  database: "q_a",
-  password: "password",
-  port: 5432,
-});
+const client = new Client(config);
