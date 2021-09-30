716684ee734f0fef364fda5efe516f3de168a939
Cory Ellerbroek <cory.ellerbroek@gmail.com>
2021-09-30 10:51:17 -0500

    Successfully loads schema and ETL for all datasets

--- etl/etl_answers.sql
+++ etl/etl_answers.sql
@@ -1,7 +1,7 @@
 \c q_a
 
-\COPY answers (id, product_id, body, epoch_date, asker_name, asker_email, reported, question_helpfulness) from '/Users/coryellerbroek/Desktop/HackReactor/sdc-questions-and-answers/datasets/sample_answers.csv' DELIMITER ',' CSV HEADER;
+\COPY answers (id, question_id, body, epoch_date, answerer_name, answerer_email, reported, helpfulness) from '/Users/coryellerbroek/Desktop/HackReactor/sdc-questions-and-answers/datasets/sample_answers.csv' DELIMITER ',' CSV HEADER;
 
-UPDATE questions SET date = to_timestamp(floor(epoch_date / 1000));
+UPDATE answers SET date = to_timestamp(floor(epoch_date / 1000));
 
-ALTER TABLE questions DROP COLUMN epoch_date;
+ALTER TABLE answers DROP COLUMN epoch_date;
--- etl/etl_questions.sql
+++ etl/etl_questions.sql
@@ -1,6 +1,6 @@
 \c q_a
 
-\COPY questions (id, product_id, question_body, epoch_date, asker_name, asker_email, reported, question_helpfulness) from '/Users/coryellerbroek/Desktop/HackReactor/sdc-questions-and-answers/datasets/questions.csv' DELIMITER ',' CSV HEADER;
+\COPY questions (id, product_id, question_body, epoch_date, asker_name, asker_email, reported, question_helpfulness) from '/Users/coryellerbroek/Desktop/HackReactor/sdc-questions-and-answers/datasets/sample_questions.csv' DELIMITER ',' CSV HEADER;
 
 UPDATE questions SET date = to_timestamp(floor(epoch_date / 1000));
 
