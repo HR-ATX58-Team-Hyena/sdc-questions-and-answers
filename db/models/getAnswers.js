b60cbce65dd68444933c0339e32cc20211225c9d
Cory Ellerbroek <cory.ellerbroek@gmail.com>
2021-10-05 10:15:47 -0500

    Minor change to getAnswers

--- db/models/getAnswers.js
+++ db/models/getAnswers.js
@@ -7,11 +7,12 @@ const getAnswers = (questionId, page = 0, count = 5, callback) => {
   const answersParams = [questionId, offset, count];
   const getAnswersQueryString = `
   SELECT
-    answer_id, body, date, answerer_name, helpfulness
+    answers.answer_id, body, date, answerer_name, helpfulness, photos.id, photos.url
   FROM
     questions
   INNER JOIN
     answers ON questions.question_id = answers.question_id
+  LEFT OUTER JOIN photos ON answers.answer_id = photos.answer_id
   WHERE
     questions.question_id = $1
   AND
@@ -26,44 +27,64 @@ const getAnswers = (questionId, page = 0, count = 5, callback) => {
     if (err) {
       callback(err, null);
     } else {
-      if (answersData.rows.length > 0) {
-        answersData.rows.forEach((answer) => {
-          answersList.push({ ...answer, photos: [] });
-          photosParams.push(answer.answer_id);
-        });
-        const getAnswersPhotos = `
-          SELECT
-            answers.answer_id, photos.id, photos.url
-          FROM
-            answers
-          INNER JOIN photos ON answers.answer_id = photos.answer_id
-          WHERE
-            answers.answer_id IN (${photosParams.join(', ')});
-        `;
-        pool.query(getAnswersPhotos, (err, photosData) => {
-          if (err) {
-            callback(err, null);
-          } else {
-            answersList.forEach((answer) => {
-              photosData.rows.forEach((photo) => {
-                if (answer.answer_id === photo.answer_id) {
-                  answer.photos.push({
-                    id: photo.id,
-                    url: photo.url,
-                  });
-                }
-              });
-            });
-          }
-          const res = {
-            question: questionId,
-            page,
-            count,
-            results: answersList,
-          };
-          callback(null, res);
-        });
-      }
+      // if (answersData.rows.length > 0) {
+      //   answersData.rows.forEach((answer) => {
+      //     answersList.push({ ...answer, photos: [] });
+      //     photosParams.push(answer.answer_id);
+      //   });
+      //   const getAnswersPhotos = `
+      //     SELECT
+      //       answers.answer_id, photos.id, photos.url
+      //     FROM
+      //       answers
+      //     INNER JOIN photos ON answers.answer_id = photos.answer_id
+      //     WHERE
+      //       answers.answer_id IN (${photosParams.join(', ')});
+      //   `;
+      //   pool.query(getAnswersPhotos, (err, photosData) => {
+      //     if (err) {
+      //       callback(err, null);
+      //     } else {
+      //       answersList.forEach((answer) => {
+      //         photosData.rows.forEach((photo) => {
+      //           if (answer.answer_id === photo.answer_id) {
+      //             answer.photos.push({
+      //               id: photo.id,
+      //               url: photo.url,
+      //             });
+      //           }
+      //         });
+      //       });
+      //     }
+      //     const res = {
+      //       question: questionId,
+      //       page,
+      //       count,
+      //       results: answersList,
+      //     };
+      //     callback(null, res);
+      //   });
+      // }
+      let response = [];
+      answersData.rows.forEach((answerData) => {
+        let answer = {};
+        answer.answer_Id = answerData.answer_id;
+        answer.body = answerData.body;
+        answer.date = answerData.date;
+        answer.answerer_name = answerData.answerId;
+        answer.helpfulness = answerData.helpfulness;
+        answer.photos;
+      });
+      const res = {
+        question: questionId,
+        page,
+        count,
+        results: {
+          and,
+        },
+      };
+      console.log(answersData);
+      callback(null, answersData);
     }
   });
 };
