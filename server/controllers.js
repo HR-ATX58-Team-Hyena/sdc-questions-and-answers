f26eebfae8654120c77889d608994132302a51e3
Cory Ellerbroek <cory.ellerbroek@gmail.com>
2021-10-06 10:57:47 -0500

    Adds file to extract controllers

---
+++ server/controllers.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('../db');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
